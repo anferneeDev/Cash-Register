5a10afaf54b56cb86f26d89c6b48e0925837eb55
ANFERNEE ABRIL RUPERTO <Aabril4810@intersg.edu>
2019-09-05 11:35:14 -0700

    Star Center Form

--- closeForm/Form1.vb
+++ closeForm/Form1.vb
@@ -1,13 +1,15 @@
 ﻿Public Class Form1
     Private Sub open_Click(sender As Object, e As EventArgs) Handles open.Click
+
         Me.Hide()
 
-        Dim sf As New Form2
-        If sf.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+        Dim f2 As New Form2
+
+        If f2.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
             'This code Close the form when form1 is open'
-      'pP'
+            'f1.Hide();'
+
         End If
         Me.Close()
     End Sub
-
 End Class
