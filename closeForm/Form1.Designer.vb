d58b14d7e53ecd133f601bb46c7125c5b47c1cbc
ANFERNEE ABRIL RUPERTO <Aabril4810@intersg.edu>
2019-09-06 10:12:30 -0400

    Probando cosas que no funcionaron

--- closeForm/Form1.Designer.vb
+++ closeForm/Form1.Designer.vb
@@ -23,31 +23,52 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.open = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'open
         '
-        Me.open.Location = New System.Drawing.Point(311, 53)
+        Me.open.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.open.Location = New System.Drawing.Point(356, 217)
         Me.open.Margin = New System.Windows.Forms.Padding(2)
         Me.open.Name = "open"
-        Me.open.Size = New System.Drawing.Size(66, 29)
+        Me.open.Size = New System.Drawing.Size(105, 45)
         Me.open.TabIndex = 0
         Me.open.Text = "open"
         Me.open.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.AutoSize = True
+        Me.Button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button3.Location = New System.Drawing.Point(0, 538)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(804, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.AutoSize = True
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(804, 561)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.open)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents open As Button
+    Friend WithEvents Button3 As Button
 End Class
